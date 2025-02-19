286c5e67e8503835b2814b5f3cb21cc8d700c788
Vladislav <you@example.com>
2025-02-19 11:32:04 +0000

    Script install SQL slave added

---
+++ install_sql_slave.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+#Установка SQL
+apt install mysql-server-8.0 -y;
+
+#Запускаем скрипт безопасности  (готовый файл с скриптом)
+./mysql_secure;
+
+# Запускаем SQL
+systemctl start mysql;
+
+# Переносим конфиг файл
+cat ./mysqld.conf > /etc/mysql/mysql.conf.d/mysqld.cnf;
+
+# Настройка и запуск репликации
+MYSQL='mysql --user=root --password=Testpass1$ --skip-column-names -e'
+
+$MYSQL "STOP REPLICA";
+$MYSQL "CHANGE REPLICATION SOURCE TO SOURCE_HOST='192.168.42.190', SOURCE_USER='repl', SOURCE_PASSWORD='oTUSlave#2020', SOURCE_AUTO_POSITION = 1, GET_SOURCE_PUBLIC_KEY = 1";
+$MYSQL "START REPLICA";
+
+# Перезапустить SQL
+systemctl restart mysql;
