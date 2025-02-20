7464f0b026fc1d9b6ef001f643b968e87021825e
TarlapanVladislav <101522035+TarlapanVladislav@users.noreply.github.com>
2025-02-20 13:38:55 +0300

    Delete install_node_exporter.sh

--- install_node_exporter.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-apt update -y;
-
-apt install node_exporter -y;
-
-systemctl daemon-reload; systemctl start node_exporter; systemctl enable node_exporter;
-
-echo "node_exporter установлен и запущен"
